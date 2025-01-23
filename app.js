4ec5c5b163f0fb24c325a6685a394f60a19ecd92
abhash157 <limbuabhash000@gmail.com>
2025-01-23 21:02:00 +0545

    debug: fixed packages and modules

--- app.js
+++ app.js
@@ -13,3 +13,8 @@ app.use("/client", express.static(__dirname + "/client"));
 
 serv.listen(2000);
 console.log("Server started.");
+
+var SOCKET_LIST = {};
+
+var io = require("socket.io")(serv, {});
+io.sockets.on("connection", function (socket) {});
---
+++ client/script.js
@@ -0,0 +1,10 @@
+const io = require("socket.io");
+
+const socket = io();
+
+let boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    socket.emit("box_click", { id: box.id });
+  });
+});
